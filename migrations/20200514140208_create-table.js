57c25f651024fd991afb6147d678983b150cfaa6
janecyyu <janecyyu@gmail.com>
2020-05-14 15:04:18 -0700

    add seeds

--- migrations/20200514140208_create-table.js
+++ migrations/20200514140208_create-table.js
@@ -1,17 +1,17 @@
 exports.up = function (knex) {
   return knex.schema
-    .createTable("recipe_details", (recipe_detail) => {
-      recipe_detail.increment();
+    .createTable("recipe_details", (recipe_details) => {
+      recipe_details.increments();
 
-      recipe_detail.string("Dish Name", 255).notNullable();
+      recipe_details.string("Dish Name", 255).notNullable();
     })
     .createTable("products", (products) => {
-      products.increment();
+      products.increments();
 
-      products.toString("Name", 255).notNullable();
+      products.string("Name", 255).notNullable();
     })
     .createTable("recipe_ingredients", (recipe_ingredients) => {
-      recipe_ingredients.increment();
+      recipe_ingredients.increments();
 
       recipe_ingredients.integer("Quantity").notNullable();
 
@@ -27,14 +27,14 @@ exports.up = function (knex) {
         .integer("IngredientsID")
         .unsigned()
         .references("id")
-        .inTable("recipe_ingredients")
+        .inTable("products")
         .onUpdate("CASCADE") //restrict, do nothing, set null, cascade
         .onDelete("RESTRICT");
     })
     .createTable("recipe_steps", (recipe_steps) => {
-      recipe_steps.increment();
+      recipe_steps.increments();
 
-      recipe_steps.toString("Instructions", 255).notNullable();
+      recipe_steps.string("Instructions", 255).notNullable();
       recipe_steps.integer("Order").notNullable();
       recipe_steps
         .integer("RecipeID")
